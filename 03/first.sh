e1f283572ecb1a9a374648c055d7efb7bbcea4f4
tkirishima <tkirishima@proton.me>
2024-12-03 19:12:02 +0000

    just bonus

--- 03/first.sh
+++ 03/first.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mul()(echo $[$1 * $2])
+mul()(echo $[$1*$2])
 
 eval "echo \$[\`$(rg -N -o "mul\(\d+,\d+\)" big.txt | tr ',()' ' ' | sd '\n' '`+`' | head -c -2)]"
--- 03/second.sh
+++ 03/second.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-mul()(echo $[$1 * $2])
+mul()(echo $[$1*$2])
 
 for match in $(rg -N -o "(mul\(\d+,\d+\))|do\(\)|don't\(\)" big.txt); {
     if [ "$match" = 'do()' ]; then
-        mul()(echo $[$1 * $2])
+        mul()(echo $[$1*$2])
     elif [ "$match" = "don't()" ]; then
         mul()(echo 0)
     else
