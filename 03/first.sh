1c591f662b55a82de27e0ac2f8fa89bd6fd510dc
tkirishima <tkirishima@proton.me>
2024-12-03 18:58:52 +0000

    compress

--- 03/first.sh
+++ 03/first.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-mul(){
-    echo $[$1 * $2]
+mul(){ echo $[$1 * $2]
 }
 
-while read line; do
-    for line in $(rg -o "mul\(\d+,\d+\)" <<< $line); do
-        mul=$[mul + `eval "$( tr ',()' ' ' <<< "$line")"`]
-    done
-done<big.txt
-
-echo "$mul"
+eval "echo \$[\`$(rg -N -o "mul\(\d+,\d+\)" big.txt | tr ',()' ' ' | sd '\n' '`+`' | head -c -2)]"
