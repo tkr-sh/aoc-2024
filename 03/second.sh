7565cd45a88f8b6384088a48bc4947813e9f255d
tkirishima <tkirishima@proton.me>
2024-12-03 19:01:29 +0000

    reduce

--- 03/second.sh
+++ 03/second.sh
@@ -4,20 +4,18 @@ mul(){
     echo $[$1 * $2]
 }
 
-while read line; do
-    for line in $(rg -o "(mul\(\d+,\d+\))|do\(\)|don't\(\)" <<< $line); do
-        if [ "$line" = 'do()' ]; then
-            mul(){
-                echo $[$1 * $2]
-            }
-        elif [ "$line" = "don't()" ]; then
-            mul(){
-                echo 0
-            }
-        else
-            mul=$[mul + `eval "$( tr ',()' ' ' <<< "$line")"`]
-        fi
-    done
-done<big.txt
+for match in $(rg -N -o "(mul\(\d+,\d+\))|do\(\)|don't\(\)" < big.txt); do
+    if [ "$match" = 'do()' ]; then
+        mul(){
+            echo $[$1 * $2]
+        }
+    elif [ "$match" = "don't()" ]; then
+        mul(){
+            echo 0
+        }
+    else
+        t=$[t + `eval "$( tr ',()' ' ' <<< "$match")"`]
+    fi
+done
 
-echo "$mul"
+echo "$t"
