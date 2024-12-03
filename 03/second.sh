021f5a2f5f2939ad9f31cf303f78ae92e60de330
tkirishima <tkirishima@proton.me>
2024-12-03 19:09:26 +0000

    reduce

--- 03/second.sh
+++ 03/second.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
-mul(){
-    echo $[$1 * $2]
-}
+mul()(echo $[$1 * $2])
 
-for match in $(rg -N -o "(mul\(\d+,\d+\))|do\(\)|don't\(\)" < big.txt); do
+for match in $(rg -N -o "(mul\(\d+,\d+\))|do\(\)|don't\(\)" big.txt); do
     if [ "$match" = 'do()' ]; then
-        mul(){
-            echo $[$1 * $2]
-        }
+        mul()(echo $[$1 * $2])
     elif [ "$match" = "don't()" ]; then
-        mul(){
-            echo 0
-        }
+        mul()(echo 0)
     else
-        t=$[t + `eval "$( tr ',()' ' ' <<< "$match")"`]
+        t=$[t + `eval $(tr ',()' ' ' <<< "$match")`]
     fi
 done
 
