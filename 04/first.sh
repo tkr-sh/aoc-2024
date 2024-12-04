329836817fca122a45ef3b7027800d6bbc32cc0a
tkirishima <tkirishima@proton.me>
2024-12-04 23:47:27 +0000

    refactor

--- 04/first.sh
+++ 04/first.sh
@@ -7,34 +7,16 @@ len=`head -n 1 <<< "$m" | wc -c`
 cnt()(a=`rg --count-matches XMAS <<< "$1"`;echo ${a:-'0'})
 transpose()(seq 1 $len | xargs -I {} sh -c "echo \"$1\" | sd '(.)' '\\\$1 ' | cut -d' ' -f {} | xargs echo")
 
-
 for _ in `seq 0 3`; do
-
-    a=`echo "$m" | sd '(.)' '\$1 '`
-    diag_m=`awk '{ print $0, "|"}' <<< $a`
-
+    diag_m=`sd '(.)' '\$1 ' <<< $m | awk '{ print $0, "|"}'`
     for i in `seq 1 $len`; do 
-        diag=`awk '{ print $NR }' <<< $diag_m | tr -d '\n'`
-        t=$[t + `cnt $diag`]
+        t=$[t + `cnt $(awk '{ print $NR }' <<< $diag_m | tr -d '\n')`]
         diag_m=`awk '{ l=$NF; $NF=""; print l,$0 }' <<< $diag_m`
-        echo "$diag" | rg 'XMAS' -C 10
     done
 
-
-    m_cut=`tr -d ' ' <<< "$a"`
-    v=`cnt "$m_cut"`
-    t=$[ t + v]
-
-    echo "=-=-=-="
-    echo "$m"
-    echo "---"
-    echo "$m" | rg 'X M A S' -C 10
-    echo "=-=-=-="
-
-    echo "wtf $t wtf"
+    t=$[t + `cnt "$m"`]
 
     m=`transpose "$(tac <<< "$m")" | tr -d ' '`
 done
-
 
 echo "$t"
--- 04/second.sh
+++ 04/second.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env bash
 
+# Note: this takes a LOOOOOOONGGGGG time
+
 m=`<big.txt`
 len=$[`head -n 1 <<< "$m" | wc -c` - 1]
 match_len=$[len - 3]
 
-
-
 transpose()(seq 1 $len | xargs -I {} sh -c "echo \"$1\" | sd '(.)' '\\\$1 ' | cut -d' ' -f {} | xargs echo")
 
-
-
-
 for _ in `seq 0 3`; do
-
     oneline=`tr -d '\n' <<< "$m"`
     for i in `seq 0 $[(len)*(len)]`; do
-        # echo "${oneline:$i:100}"
         if echo "${oneline:$i:500}" | rg "^M.S(.{$match_len}).A.(.{$match_len})M.S"; then
-            echo "$t!!!"
             t=$[t+1]
         fi
     done
 
-    echo "$m"
     m=`transpose "$(tac <<< "$m")" | tr -d ' '`
-    echo "$m"
 done
 
 
 echo "$t"
-
