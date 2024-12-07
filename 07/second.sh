2230156ed77e8f49f219010c1e852a9f138f6da7
tkirishima <tkirishima@proton.me>
2024-12-07 12:46:14 +0000

    day7: this one was funny :)

---
+++ 07/second.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+while read line; do
+    IFS=":" read look_for numbers <<< "$line"
+
+    echo "$numbers"
+    rg -o <<< "$numbers" | wc -l
+    spaces=$[`rg -o ' ' <<< "$numbers" | wc -l` - 1]
+    go_to=`bc <<< "3^$spaces"`
+
+    from=0
+
+    while [[ $go_to -gt $from ]]; do
+        operators=`printf "%0$[spaces]d" $(dc -e "$from 3op") | tr '012' '+*|'`
+        expr_in_lines=`paste -d'\n' <(echo -e "$(tr ' ' '\n' <<< $numbers | tail -n +2)") <(echo -e "$(sd '(.)' ']$1\n' <<< $operators)") | sd '\n\]\|' ']'`
+        number=`eval echo '$['"$(printf "%${spaces}s" | sd ' ' '$[')$(tr -d '\n' <<< "$expr_in_lines")"']'`
+
+        if [[ $number = "$look_for" ]]; then
+            echo "[$tot $look_for]"
+            tot=$[tot+look_for]
+            break
+        fi
+        from=$[from + 1]
+    done
+done < big.txt
+
+echo "Total: $tot"
